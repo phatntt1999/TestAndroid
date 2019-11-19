ec76d64243f212dc82657c8868b10b5973b44020
TIến Phát <tienphattt1999@gmail.com>
2019-11-19 11:37:08 +0700

    Add project files.

---
+++ Resources/BillCalculate/BillCalculate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestWithGiasutinhocGuide.Resources.BillCalculate
+{
+    class BillCalculate
+    {
+        private int _Amount;
+        private double _Price;
+
+        public BillCalculate(int amount, double price)
+        {
+            _Amount = amount;
+            _Price = price;
+        }
+
+        public double getPrice()
+        {
+            return _Price;
+        }
+
+        public int getAmount()
+        {
+            return _Amount;
+        }
+
+        public void setPrice(double price)
+        {
+            _Price = price;
+        }
+
+        public void setAmount(int amount)
+        {
+            _Amount = amount;
+        }
+
+        public double TotalBill()
+        {
+            return _Price * _Amount;
+        }
+    }
+}
---
+++ Resources/Profile/Profile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestWithGiasutinhocGuide.Resources.Profile
+{
+    class Profile
+    {
+        private string _Name;
+        private int _Age;
+
+        public Profile (string name, int age)
+        {
+            _Name = name;
+            _Age = age;
+        }
+
+        public string getName()
+        {
+            return _Name;
+        }
+
+        public int getAge()
+        {
+            return _Age;
+        }
+    }
+}
